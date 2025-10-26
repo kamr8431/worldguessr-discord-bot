d79b6d42ce166a5021848081de3a837fe114145f
kamr8431 <rishikamath09@gmail.com>
2025-10-25 21:43:24 -0700

    Update letsplay.js

--- commands/letsplay.js
+++ commands/letsplay.js
@@ -1,12 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path'); 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('letsplay')
-        .setDescription('Use this command to find others to play Worldguessr with!'),
+        .setDescription('Use this command to find others to play Worldguessr with!')
+        .addIntegerOption(o =>
+            o.setName('code')
+              .setDescription('Enter your Worldguessr game code')
+              .setRequired(true)
+          ),
 
     async execute(interaction) {
-        const gameCode = interaction.options.getInteger('code');
+        const gameCode = interaction.options.getInteger('game code',true);
         const userId = interaction.user.id;
         const roleId = 1421434087343657093;
         const filePath = path.join(__dirname, '..', 'letsplaypings.json');
@@ -18,9 +25,9 @@ module.exports = {
             console.log('Error reading letsplaypings.json');
             data = {};
         }
-        if (userId && Date.now() - date[userId].timestamp < 3600000) {
+        if (userId in data && Date.now() - data[userId].timestamp < 3600000) {
             await interaction.reply({
-                content: 'Please wait ${Math.ceil((3600000 - (Date.now() - data[userId].timestamp)) / 60000)} minutes before using this command again.'
+                content: `Please wait ${Math.ceil((3600000 - (Date.now() - data[userId].timestamp)) / 60000)} minutes before using this command again.`
             });
         } else {
             data[userId] = {
@@ -29,7 +36,7 @@ module.exports = {
 
             fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
             await interaction.reply({
-                content: '<@${roleId}>, <@${userId}> wants to play at ${gameCode}'
+                content: `<@&${roleId}>, <@${userId}> wants to play at ${gameCode}`
             });
         }
     }
